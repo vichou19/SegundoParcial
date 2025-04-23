3734d5a43c4df4c657c42a8c9a433439b114a488
vichou19 <vi.alcaide@duocuc.cl>
2025-04-23 15:03:15 -0400

    Add files via upload

---
+++ importacion math,                    tarea por terminar.py
@@ -0,0 +1,26 @@
+"""
+el casino virtual de santiago tiene un nuevo juego, que nos permite ingresar numeros ENTEROS
+luego establecemos el rango para establecer cual es el mayor y o el menor si nosotros logramos adivinr el numero que 
+sigue alazar nos ganariamos $1.000.000
+para este ejercicio importar la siguiente libreria, Importe range
+"""
+
+#hacer ben otra clase o terminar en la tarde
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+#importaciones
+
+
+#import math
+
+#print(math.sqrt(10))
+
+import numpy as np
+
+array = np.array([1,3,4])
+
+print(array)
