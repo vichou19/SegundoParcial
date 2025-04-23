5c58d8c3bf060f9fc2042d1d1e2574caac0accb7
vichou19 <vi.alcaide@duocuc.cl>
2025-04-23 14:23:57 -0400

    Add files via upload

---
+++ clase def.py
@@ -0,0 +1,9 @@
+numero1 = int(input("dime un numero: "))
+numero2 = int(input("dime otro numero: "))
+
+def Suma(numero1,numero2):
+    return numero1 + numero2
+
+Resultado = Suma(numero1 , numero2)
+
+print("el resultado es:", Resultado)
