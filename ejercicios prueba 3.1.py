210adfb9bd202158d504db2d0204633f60c304f8
vichou19 <vi.alcaide@duocuc.cl>
2025-05-22 15:34:56 -0400

    Add files via upload

---
+++ ejercicios prueba 3.1.py
@@ -0,0 +1,46 @@
+#1 ejercicio practica
+
+"""
+LA GENTE DE DIREC TV NESECITA MEJORAR SU MENU DE OPCIONES POR ENDE ELLOS NOS PRESENTAN
+EL SIGUIENTE MENU
+1 Para navegar dentro de las peliculas
+2 Para grabar una pelicula
+3 Configurar decodificador
+4 Para salir
+"""
+
+#2 ejercicio para la prueba 3(●'◡'●)
+
+"""
+Haga un menu que permita hacer la siguiente function
+menu principar
+1 ingrese un numero
+2 mostrar mayor
+3 mostrar promedio
+4 salir del ejercicio
+
+opcion numero 1/ debe permitir ingresar un numero entero entre el 0 y el 100
+por excepcion ya que incluso podria ingresar un string o un numero si el usuario no
+ingresaun numero entero deberia sollicitar en pantalla (debe ingresar un numero entero)
+
+opcion 2/ debe mostrar el numero mayor ingresadohasta ese momento
+opcion 3/ debe mostrar el promedio ingresado hasta ese momento
+si no se ah ingresado ningun numero, pedir estimado ingrese primero el valor
+
+opcion 4/ salir, dar un mensaje de adios
+"""
+
+#3 ejercicio para prueba 3
+
+"""
+contruya un programa en python que permita verificar si una persona a completado
+su esquema de vacunacion, el programa debe comenzar preguntandole
+ingresa un numero indicando la cantidad de personas a ingresar, el numero debe ser entero
+luego para cada persona generar el registro
+el registro a nuestra discrecion
+el programa debe solicitar la dosis que an sido administraba
+esquema completo en caso contrario con ✖️, y si no ✅
+una vez ingresado los datos a mostrar en pantalla
+deberia tener una opcion para entrar y salir
+validar si pone un decimal mandar mensaje( ingresa numero entero)
+"""
