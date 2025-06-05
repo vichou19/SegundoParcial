513cefba5259002f0f1952c8481d6c5c4255adf9
vichou19 <vi.alcaide@duocuc.cl>
2025-06-05 15:14:25 -0400

    Add files via upload

---
+++ ejerciciooooooooo.py
@@ -0,0 +1,6 @@
+"""
+las personas de sansum buscan un desarrollador un python que sea capas de hacer la agenda de su ultimo telefono
+la cual debe ser capas de registrar nombre y apellido, direccion, fecha de cumpleaños, correo y telefono.
+ademas debe ser capas de ser acsesible para consultar los contactos ingresados, eliminar, modificar, actualizar y guardar.
+
+"""
